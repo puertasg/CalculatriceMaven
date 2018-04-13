e58a60f9624bb148ea3e32e99d5bfda788cf4300
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-13 11:36:49 +0200

    Ajout javadoc pour chaque méthode dans ApplicationTest

--- src/test/java/org/formation/java/ApplicationTest.java
+++ src/test/java/org/formation/java/ApplicationTest.java
@@ -18,11 +18,19 @@ public class ApplicationTest extends TestCase{
 		return new TestSuite(ApplicationTest.class);
 	}
 	
+	/**
+	 * Vérifie le comportement de la méthode addition, si elle additionne bien a et b
+	 */
+	
 	public void testAddition()
 	{
 		Calculatrice c = new Calculatrice();
 		assertTrue(c.addition(1, 2) == 3);
 	}
+	
+	/**
+	 * Vérifie le comportement de la méthode soustraction, si elle soustrait bien a et b
+	 */
 	
 	public void testSoustraction()
 	{
@@ -30,17 +38,30 @@ public class ApplicationTest extends TestCase{
 		assertTrue(c.soustraction(4, 1) == 3);
 	}
 	
+	/**
+	 * Vérifie le comportement de la méthode multiplication, si elle multiplie bien a et b
+	 */
+	
 	public void testMultiplication()
 	{
 		Calculatrice c = new Calculatrice();
 		assertTrue(c.multiplication(5, 2) == 10);
 	}
 	
+	/**
+	 * Vérifie le comportement de la méthode division, si elle divise bien a et b
+	 */
+	
 	public void testDivision()
 	{
 		Calculatrice c = new Calculatrice();
 		assertTrue(c.division(8, 2) == 4);
 	}
+	
+	/**
+	 * Vérifie le comportement de la méthode division en cas de division par zéro, 
+	 * si on catch ArithmeticException correctement
+	 */
 	
 	public void testDivisionParZero()
 	{
