1ae27ab8060c19ea8a3a7610a7006864b3050ea6
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-13 11:26:23 +0200

    Ajout javadoc pour chaque méthode de Calculatrice

--- src/main/java/calculatricemaven/Calculatrice.java
+++ src/main/java/calculatricemaven/Calculatrice.java
@@ -15,20 +15,56 @@ public class Calculatrice {
         
     }
     
+    /**
+     * 
+     * @param a int, nombre entier passé en argument
+     * @param b int, nombre entier passé en argument
+     * @return int, résultat de l'opération
+     * 
+     * Prends deux int en entrée et retourne le résultat de leur addition
+     */
+    
     public int addition(int a, int b)
     {
         return a+b;
     }
+    
+    /**
+     * 
+     * @param a int, nombre entier passé en argument
+     * @param b int, nombre entier passé en argument
+     * @return int, résultat de l'opération
+     * 
+     * Prends deux int en entrée et retourne le résultat de leur multiplication
+     */
     
     public int multiplication(int a, int b)
     {
         return a*b;
     }
     
+    /**
+     * 
+     * @param a int, nombre entier passé en argument
+     * @param b int, nombre entier passé en argument
+     * @return int, résultat de l'opération
+     * 
+     * Prends deux int en entrée et retourne le résultat de leur soustraction
+     */
+    
     public int soustraction(int a, int b)
     {
         return a-b;
     }
+    
+    /**
+     * 
+     * @param a int, nombre entier passé en argument
+     * @param b int, nombre entier passé en argument
+     * @return int, résultat de l'opération
+     * 
+     * Prends deux int en entrée et retourne le résultat de leur division
+     */
     
     public int division(int a, int b)
     {
